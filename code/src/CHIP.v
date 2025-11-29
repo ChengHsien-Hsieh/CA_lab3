bd3902ac65a0e959a21801bb9ff4e9528d9fc570
ChengHsien-Hsieh <stevensian27@gmail.com>
2025-11-30 00:06:16 +0800

    Fix nested git submodule problem

---
+++ code/src/CHIP.v
@@ -0,0 +1,53 @@
+//----------------------------- DO NOT MODIFY THE I/O INTERFACE!! ------------------------------//
+module CPU #(                                                                                  //
+    parameter BIT_W = 32                                                                        //
+)(                                                                                              //
+    // clock                                                                                    //
+        input               i_clk,                                                              //
+        input               i_rst_n,                                                            //
+    // instruction memory                                                                       //
+        input  [BIT_W-1:0]  i_IMEM_data,                                                        //
+        output [BIT_W-1:0]  o_IMEM_addr,                                                        //
+        output              o_IMEM_cen,                                                         //
+    // data memory                                                                              //
+        input               i_DMEM_stall,                                                       //
+        input  [BIT_W-1:0]  i_DMEM_rdata,                                                       //
+        output              o_DMEM_cen,                                                         //
+        output              o_DMEM_wen,                                                         //
+        output [BIT_W-1:0]  o_DMEM_addr,                                                        //
+        output [BIT_W-1:0]  o_DMEM_wdata,                                                       //
+    // finnish procedure                                                                        //
+        output              o_finish,                                                           //
+    // cache                                                                                    //
+        input               i_cache_finish,                                                     //
+        output              o_proc_finish                                                       //
+);                                                                                              //
+//----------------------------- DO NOT MODIFY THE I/O INTERFACE!! ------------------------------//
+
+    // TODO: Reg_file wire connection
+    Reg_file reg0(               
+        .i_clk  (i_clk),             
+        .i_rst_n(i_rst_n), 
+        // input        
+        .wen    (),          
+        .rs1    (),                
+        .rs2    (),                
+        .rd     (),                 
+        .wdata  (),             
+        .rdata1 (),           
+        .rdata2 ()
+    );
+
+// -------------------------------------------------------
+// PC part
+// -------------------------------------------------------
+    reg [BIT_W-1:0] PC;
+    always @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n) begin
+            PC <= 32'h00010000; // Do not modify this value!!!
+        end
+        else begin
+            PC <= 
+        end
+    end
+endmodule
